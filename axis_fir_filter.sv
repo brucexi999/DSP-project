8fd53cadae99df2a63aea417705572a26b373131
Bruce Xi <91307696+brucexi999@users.noreply.github.com>
2024-05-08 18:34:37 -0700

    prototype done

--- axis_fir_filter.sv
+++ axis_fir_filter.sv
@@ -21,13 +21,17 @@ module axis_fir_filter #(
     output s_axis_tready
 );
     logic signed [DATA_OUT_WIDTH-1:0] fir_data_out;
-    logic signed [DATA_OUT_WIDTH-1:0] data_skid_buffer; 
+    logic signed [DATA_OUT_WIDTH-1:0] data_skid_buffer;
+    logic last_fir;
+    logic last_skid_buffer;
     logic m_axis_tready_reg;  // Registered ready signal to break long combinational path and improve timing
     logic skid_valid;  // Indicates when there's valid data in the skid buffer
     logic fir_valid;  // Indicates when there's valid data at the FIR filter's output
     logic [UP_COUNTER_WIDTH-1:0] up_counter;  // Used to count the # cycles between a valid FIR input and the first valid FIR output, controlling the assertion of fir_valid
     logic [DOWN_COUNTER_WIDTH-1:0] down_counter;  // Used to count the # cycles between the last FIR input and the first invalid FIR output, controlling the de-assertion of fir_valid
     logic fir_clock_en;
+    logic down_counter_en;
+    logic up_counter_rst;
 
     fir_filter #(NUM_TAPS, TAP_WIDTH, DATA_IN_WIDTH, DATA_OUT_WIDTH) FIR (
         .clk(clk),
@@ -37,38 +41,105 @@ module axis_fir_filter #(
         .data_out(fir_data_out)
     )
 
-    always@(posedge clk) begin
-        if (!rst_n)
+    always_ff@(posedge clk) begin
+        if (!rst_n) begin
             skid_valid <= 0;
-        else if (fir_valid && m_axis_tready_reg && !m_axis_tready)  // We have received data from the FIR, but the downstream is not ready, need to put the FIR data into the skid buffer
+            data_skid_buffer <= 0;
+            last_skid_buffer <= 0;
+        end
+        else if (fir_valid && m_axis_tready_reg && !m_axis_tready) begin  // We have received data from the FIR, but the downstream is not ready, need to put the FIR data into the skid buffer
             skid_valid <= 1;
-        else if (skid_valid && m_axis_tready)  // The data in the skid buffer has been taken away
-            skid_valid <= 0;
-    end
-
-    always@(posedge clk) begin
-        if (!rst_n)
-            data_skid_buffer <= 0;
-        else if (fir_valid && m_axis_tready_reg && !m_axis_tready)
             data_skid_buffer <= fir_data_out;
-        else if (skid_valid && m_axis_tready)
+            last_skid_buffer <= last_fir;
+        end
+        else if (skid_valid && m_axis_tready) begin  // The data in the skid buffer has been taken away
+            skid_valid <= 0;
             data_skid_buffer <= 0;
+            last_skid_buffer <= 0;
+        end
     end
 
-    always@(posedge clk) begin
-        if (!rst_n) // reset when the last FIR output has been taken away
+    typedef enum {
+        reset_state
+        idle_state,
+        operation_state,
+        down_counter_state,
+        send_last_state
+    } state_type;
+
+    state_type current_state, next_state;
+
+    always_ff@(posedge clk) begin
+        if (!rst_n)
+            current_state <= reset_state;
+        else
+            current_state <= next_state;
+    end
+
+    always_comb begin
+        last_fir = 0;
+        up_counter_rst = 0;
+        down_counter_en = 0;
+        case (current_state)
+            reset_state: begin
+                if (!rst_n)
+                    next_state = reset_state;
+                else
+                    next_state = idle_state;
+            end
+            idle_state: begin
+                up_counter_rst = 1;
+                if (m_axis_tready_reg && s_axis_tvalid)  // If the first sample has enterred the system, we enter the operation state
+                    next_state = operation_state;
+                else
+                    next_state = idle_state;
+            end
+            operation_state: begin
+                if (m_axis_tready_reg && s_axis_tvalid && s_axis_tlast)
+                    next_state = down_counter_state;
+                else
+                    next_state = operation_state;
+            end
+            down_counter_state: begin
+                down_counter_en = 1;
+                if (down_counter == 0)
+                    next_state = send_last_state;
+                else
+                    next_state = down_counter_state;
+            end
+            send_last_state: begin
+                last_fir = 1;
+                if (m_axis_tready_reg && fir_valid)  // If the last sample has not been taken, stay in this state
+                    next_state = idle_state;
+                else
+                    next_state = send_last_state;
+            end
+            default: next_state = operation_state;
+        endcase
+    end
+
+    
+    
+    always_ff@(posedge clk) begin
+        if (!rst_n || up_counter_rst)  // Reset when the last FIR output has been taken away
             up_counter <= 0;
-        else if (m_axis_tready_reg && s_axis_tvalid && up_counter != UP_COUNTER_MAX)
+        else if (m_axis_tready_reg && s_axis_tvalid && up_counter != UP_COUNTER_MAX)  // Once the up counter reaches the max, it means the first output signal has come out, from now on, if the input remains valid, the output remains valid
             up_counter <= up_counter + 1;
     end
 
-    assign fir_valid = up_counter == UP_COUNTER_MAX;
+    assign fir_valid = (up_counter == UP_COUNTER_MAX) && s_axis_tvalid;  // Whenever the upstream de-asserts s_axis_tvalid, the FIR pipeline halts by de-asserted fir_clock_en, at the mean time, fir_valid will be deasserted
 
-    // Whenever the upstream de-asserts s_axis_tvalid, the FIR pipeline halts by de-asserted fir_clock_en, at the mean time, fir_valid will be deasserted
+    always_ff@(posedge clk) begin
+        if (!rst_n || down_counter == 0)
+            down_counter <= DOWN_COUNTER_MAX;
+        else if (m_axis_tready_reg && s_axis_tvalid && down_counter_en)
+            down_counter <= down_counter - 1;
+    end
     // When s_axis_tlast is observed, the down counter will be activated to correctly deasserts fir_valid and generate last signal to the downstream, at the same time, s_axis_tready should be deasserted to reject input from a new signal
 
     assign m_axis_tready_reg = !skid_valid; 
     assign m_axis_tdata = skid_valid ? data_skid_buffer : fir_data_out;
+    assign m_axis_tlast = skid_valid ? last_skid_buffer : last_fir;
     assign s_axis_tready = m_axis_tready_reg;
     assign fir_clock_en = m_axis_tready_reg && s_axis_tvalid;  // FIR must be enabled when there's handshake at the input
     assign m_axis_tvalid = skid_valid || fir_valid;
